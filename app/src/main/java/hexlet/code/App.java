ac623757747816dec27f1bcf5e3f1d3c3e5ed71b
Anastasia Lukash <anastasia16rom@gmail.com>
2024-05-26 22:09:54 +0200

    update build config

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,7 @@
-package java.hexlet.code;
+package hexlet.code;
 
 public class App {
     public static void main(String[] args) {
-
+        System.out.println("Welcome to the Brain Games!");
     }
 }
